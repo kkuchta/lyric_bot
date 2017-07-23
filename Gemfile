8dcf921386440257a148e6bc4c88ca56be1b2836
Kevin Kuchta <kevin@kevinkuchta.com>
2017-07-22 20:30:06 -0700

    Basic tweet streaming

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ ruby '2.3.1'
 
 gem 'chatterbot'
 gem 'dotenv'
+
+group :development do
+  gem 'pry-byebug'
+end
--- bot.rb
+++ bot.rb
@@ -1,11 +1,20 @@
 require 'dotenv'
 require 'twitter'
-Dotenv.load
-puts ENV['TEST']
 
-client = Twitter::REST::Client.new do |config|
+Dotenv.load
+
+if ENV['ENV'] == 'development'
+  require 'pry-byebug'
+end
+
+client = Twitter::Streaming::Client.new do |config|
   config.consumer_key        = ENV['CONSUMER_KEY']
   config.consumer_secret     = ENV['CONSUMER_SECRET']
   config.access_token        = ENV['ACCESS_TOKEN']
   config.access_token_secret = ENV['ACCESS_SECRET']
 end
+
+client.sample do |object|
+  puts object.text if object.is_a?(Twitter::Tweet)
+end
+puts 'done'
