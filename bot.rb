06bfd5df8a5f9021bba4479649fbae65c4df346a
Kevin Kuchta <kevin@kevinkuchta.com>
2017-07-22 20:22:32 -0700

    Initital revision

---
+++ bot.rb
@@ -0,0 +1,11 @@
+require 'dotenv'
+require 'twitter'
+Dotenv.load
+puts ENV['TEST']
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_SECRET']
+end
